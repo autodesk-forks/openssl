1fa3f1cd255e979498d27d787337f8fe3ad38111
Bekir Murat <bekir.murat@autodesk.com>
2016-02-29 02:31:19 +0800

    Make it build with VS2015 + added build script

---
+++ build_2015.bat
@@ -0,0 +1,22 @@
+@ECHO OFF
+
+IF NOT "%VisualStudioVersion%" == "14.0" (
+    ECHO You must be in a VS2015 command prompt
+    ECHO Exiting
+    Exit /B 1
+)
+
+set OPENSSL_VERSION=1.0.0t
+
+ECHO 1/6 ....
+perl Configure VC-WIN32 no-asm no-idea no-mdc2 no-rc5 --prefix=C:\openssl-%OPENSSL_VERSION%
+ECHO 2/6 ....
+ms\do_ms.bat
+ECHO 3/6 ....
+nmake -f ms\ntdll.mak
+ECHO 4/6 ....
+nmake -f ms\ntdll.mak install
+ECHO 5/6 ....
+nmake -f ms\nt.mak
+ECHO 6/6 ....
+nmake -f ms\nt.mak install
