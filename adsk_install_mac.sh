a29f63c10ffdb4a439d730cba70a87410e9041c0
dalalv <dalalvishal@gmail.com>
2016-03-24 03:06:30 -0700

    P4CL: 509814 From: Sync P4 depot By: Mahendra CL Desc.: OpenSSL Mac Build Script

---
+++ adsk_install_mac.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# 
+
+
+if [ .$1. = .. ]; then
+	echo usage: ./adsk_install_mac.sh  \<install_path\> \<Min OS X Version supported: eg. ., 10.8, 10,9, .\>
+	exit
+fi
+if [ .$2. = .. ]; then
+	echo usage: ./adsk_install_mac.sh  \<install_path\> \<Min OS X Version supported: eg. ., 10.8, 10,9, .\>
+	exit
+fi
+
+	rm -rf $1
+
+	chmod -RLH -f u+w .
+	make clean
+	./Configure  no-idea no-mdc2 no-rc5 no-ssl2 no-ssl3 darwin64-x86_64-cc --prefix=$1 --openssldir=$1 
+	sed -ie "s/^CFLAG= -/CFLAG=  -mmacosx-version-min=$2 -/" "Makefile"
+	make depend
+	make
+	sudo make install
+
+
